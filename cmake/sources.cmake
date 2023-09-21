cf7792a22f185150f0b10fc0bae8332ebf76f9ce
JaySi98 <jakub.szkiladz@wp.pl>
2023-09-21 21:58:43 +0200

    removed unused files

--- cmake/sources.cmake
+++ cmake/sources.cmake
@@ -2,23 +2,11 @@ set(HEADERS
     include/data/transaction_type.hpp
     include/data/monthly_budget.hpp
     include/data/options_parser.hpp
-
-    # include/view_models/base_vm.hpp
-    # include/view_models/main_vm.hpp
-    # include/view_models/reports_vm.hpp
-    # include/domain_models/base_dm.hpp
-    # include/domain_models/main_dm.hpp
-    # include/domain_models/reports_dm.hpp
 )
 
 set(SOURCES
     src/data/options_parser.cpp
     src/data/monthly_budget.cpp
-
-    # src/view_models/main_vm.cpp
-    # src/view_models/reports_vm.cpp
-    # src/domain_models/main_dm.cpp
-    # src/domain_models/reports_dm.cpp
 )
 
 set(EXE_SOURCES
