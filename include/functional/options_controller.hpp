d19e1bc9b8338cea48cd6d1a35a55c7c5b5a58be
JaySi98 <jakub.szkiladz@wp.pl>
2023-09-29 16:16:50 +0200

    unfinished options_controller

--- include/functional/options_controller.hpp
+++ include/functional/options_controller.hpp
@@ -2,6 +2,8 @@
 
 #include <boost/program_options/variables_map.hpp>
 #include <string>
+#include <data/transaction_type.hpp>
+
 
 namespace po = boost::program_options;
 
@@ -14,6 +16,16 @@ namespace functional
             void execute_option_command();
 
         private:
+            void add_transaction();
+            void print_monthly_budget();
+            void print_help();
+            void print_categories();
+
+            bool option_set(options option);
+            template<typename T>
+            T get_option_value(options option);
+
+
             const po::variables_map* m_variables;
     };
 };
--- src/functional/options_controller.cpp
+++ src/functional/options_controller.cpp
@@ -1,4 +1,8 @@
 #include <functional/options_controller.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/date_clock_device.hpp>
+
+using namespace boost::gregorian;
 
 namespace functional 
 {
@@ -8,7 +12,90 @@ namespace functional
 
     void options_controller::execute_option_command()
     {
-
+        if(option_set(options::add))
+        {
+            add_transaction();
+        }
+        else if(option_set(options::month))
+        {
+            print_monthly_budget();
+        }
+        else if(option_set(options::help))
+        {
+            print_help();
+        }
+        else if(option_set(options::types))
+        {
+            print_categories();
+        }
+    }
+    
+    bool options_controller::option_set(options option)
+    {
+        return m_variables->count(str_options[option].c_str());
     }
 
+    template<typename T>
+    T options_controller::get_option_value(options option)
+    {
+        // std::string str_option{str_options[option].c_str()};
+        // return m_variables[str_option].as<T>();
+    }
+
+    void options_controller::add_transaction()
+    {
+        date d{day_clock::universal_day()};
+        std::string note{""};
+        float value{0};
+        transaction_category category{transaction_category::unknown};
+
+        if(option_set(options::date))
+        {
+            // std::string date_str = parsed_variables["date"].as<std::string>(); 
+            // d = from_simple_string(date_str);
+        }
+
+        if(option_set(options::note))
+        {
+            // note = parsed_variables["note"].as<std::string>();
+        }
+
+        if(option_set(options::value))
+        {
+            // value = parsed_variables["value"].as<float>();
+        }
+
+        if(option_set(options::category))
+        {
+            // std::string s_type = parsed_variables["cat"].as<std::string>();
+            
+            // auto find_type = std::find_if(std::begin(str_tran_cat), std::end(str_tran_cat), [&](const std::pair<transaction_category, std::string>& pair)
+            // {
+            //     return pair.second.compare(s_type) == 0; 
+            // });
+
+            // if(find_type != std::end(str_tran_cat))
+            // {
+            //     category = find_type->first;
+            // }
+        }
+
+        transaction trans{0, d, category, note, value};
+        std::cout << trans << std::endl;
+    }
+
+    void options_controller::print_monthly_budget()
+    {
+
+    }
+    
+    void options_controller::print_help()
+    {
+
+    }
+    
+    void options_controller::print_categories()
+    {
+
+    }
 };
